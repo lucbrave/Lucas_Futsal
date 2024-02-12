5dee65b3491a11dc40924647f28bdf5fa2d4350b
Lucas Coimbra <70152716+lucbrave@users.noreply.github.com>
2024-02-12 01:44:40 -0300

    Only two times team against another team

--- app/Http/Requests/Matche/StoreRequest.php
+++ app/Http/Requests/Matche/StoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Matche;
 
 use App\Models\Matche;
+use Illuminate\Validation\Rule;
 use App\Http\Requests\BaseRequest;
 
 class StoreRequest extends BaseRequest
@@ -24,8 +25,15 @@ class StoreRequest extends BaseRequest
             'end_date' => 'required|string|max:255',
             'score_team1' => 'required|string|max:255',
             'score_team2' => 'required|string|max:255',
-            'team_1' => 'required|string',
-            'team_2' => 'required|string',
+            'team_2'      => 'required|string',
+            'team_1'      => [ 'required',Rule::unique('matches', 'team_1')->where('team_2', $this->input('team_2'))],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'team_1.unique' => 'Um time só pode jogar contra o outro duas vezes.',
         ];
     }
 }
